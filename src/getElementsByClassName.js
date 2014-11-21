60a55e3a128e95a0bee81ebadf3a3686667685e7
Travis <travisechapman@gmail.com>
2014-11-20 21:33:23 -0500

    Recursive getElementsByClassName function is recursive

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,31 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  // your code here
+  //containsTarget() checks for a string className in an arraylike classList
+  var containsTarget = function(classList, className) {
+    for (var i = 0; i < classList.length; i++) {
+        if (classList[i] === className) {
+            return true;
+        }
+    }
+    return false;
+  }
+  //global result array
+  var result = [];
+
+  //define a function that adds elements to the global array if they have className, then traverses childNodes recursively
+  var recursive = function(element) {
+    var classes = element.classList === undefined ? [] : element.classList;
+    if (containsTarget(classes, className)) {
+        result.push(element);
+    }
+    for (var i = 0; i < element.childNodes.length; i++) {
+        var child = element.childNodes[i];
+        recursive(child);
+    }
+    return result;
+  }
+
+  //call the recusive function on document.body, this returns the global result array
+  return recursive(document.body);
 };
